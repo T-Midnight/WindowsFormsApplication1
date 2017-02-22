72cc49afc81835fa17ab0893694415a0aabc713d
T-Midnight <belousovatanya1995@yandex.ru>
2017-02-22 19:53:38 +0400

    Change position of elements on form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -43,20 +46,26 @@
             // 
             // hScrollBar1
             // 
-            this.hScrollBar1.Location = new System.Drawing.Point(145, 79);
+            this.hScrollBar1.Location = new System.Drawing.Point(9, 223);
             this.hScrollBar1.Name = "hScrollBar1";
             this.hScrollBar1.Size = new System.Drawing.Size(80, 17);
             this.hScrollBar1.TabIndex = 1;
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(467, 250);
+            this.ClientSize = new System.Drawing.Size(366, 268);
             this.Controls.Add(this.hScrollBar1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,6 +75,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.FontDialog fontDialog1;
     }
 }
 
